d37afd49c52167d3ea73d6fd4c0bc2448ef2f769
ZackBoe <zacknut@gmail.com>
2014-03-02 22:18:07 -0500

    Added forecast, moved desc

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 $(function() {
 	console.log("ready to roll!");
+	desc = $('.desc');
+	forecast = $('.forecast');
+	$('.toggle-desc').click(function(e) {
+		e.preventDefault();
+		if($(this).hasClass('open')) desc.slideUp();
+		else { 
+			desc.slideDown();
+			forecast.hide();
+		}
+		$(this).toggleClass('open');
+	});
+
+	$('.taco').click(function(e) {
+		e.preventDefault();
+		if($(this).hasClass('fin')){
+			if($(this).hasClass('open')) forecast.slideUp();
+			else { forecast.slideDown(); desc.hide();
+			}
+			$(this).toggleClass('open');
+		}
+		else e.preventDefault();
+	});
+
+	$('.close').click(function(e) {
+		e.preventDefault();
+		forecast.slideUp();
+		$('.taco').toggleClass('open');
+	});
+
 if (Modernizr.geolocation) {
   getLocation();
 } else {
@@ -33,13 +62,73 @@ function getWeather (lat, lng, city) {
 	if(city != null){
 		$.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=927b95cfa440255eddf1ebff447cab15", function( data ) {
 			yoavWeather(data);
+			$.getJSON( "http://api.openweathermap.org/data/2.5/forecast/daily?q="+city+"&cnt=8&mode=json&APPID=927b95cfa440255eddf1ebff447cab15", function( data ) {
+				yoavForecast(data);
+			}); 
 		}); 
 	}
 	else {
 		$.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&APPID=927b95cfa440255eddf1ebff447cab15", function( data ) {
 			yoavWeather(data);
+			$.getJSON( "http://api.openweathermap.org/data/2.5/forecast/daily?lat="+lat+"&lon="+lng+"&cnt=8&mode=json&APPID=927b95cfa440255eddf1ebff447cab15", function( data ) {
+				yoavForecast(data);
+			}); 
 		}); 
 	}
+}
+
+
+function yoavForecast (forecast) {
+	if(forecast.cod != 200){
+
+	}
+	else {
+		for (var i = forecast.list.length - 1; i >= 0; i--) {
+			if(i == 3) $('.forecast').prepend('<div class="day spacer"></div><div class="day spacer"></div>');
+			console.log(forecast.list[i]);
+			var d = new Date(0);
+			d.setUTCSeconds(forecast.list[i].dt);
+			switch(d.getDay()){
+				case 0:
+					day = 'Sunday';
+					break;
+				case 1:
+					day = 'Monday';
+					break;
+				case 2:
+					day = 'Tuesday';
+					break;
+				case 3:
+					day = 'Wednesday';
+					break;
+				case 4:
+					day = 'Thursday';
+					break;
+				case 5:
+					day = 'Friday';
+					break;
+				case 6:
+					day = 'Saturday';
+					break;
+				default:
+					day = 'Someday';
+					break;
+			}
+			maxtC = forecast.list[i].temp.max - 273.15;
+			maxtF = maxtC * 9 / 5 + 32;
+			maxtempF = maxtF.toString().split(".");
+			maxtempC = maxtC.toString().split(".");
+			mintC = forecast.list[i].temp.min - 273.15;
+			mintF = mintC * 9 / 5 + 32;
+			mintempF = mintF.toString().split(".");
+			mintempC = mintC.toString().split(".");
+			icon =  forecast.list[i].weather[0].icon;
+			$('.forecast').prepend('<div class="day"><h3>'+day+'</h3><img src="http://openweathermap.org/img/w/'+icon+'.png"><h4><span class="h">'+maxtempF[0]+'&deg;</span>&nbsp;&nbsp;<span class="l">'+mintempF[0]+'&deg; F</span></h4><h4><span class="h">'+maxtempC[0]+'&deg;</span>&nbsp;&nbsp;<span class="l">'+mintempC[0]+'&deg; C</span></h4></div>')
+			date = null;
+		};
+	}
+	// $('.forecast').append('<div class="day close toggle-forecast">Close</div>');
+	$('.taco').addClass('fin');
 }
 
 function yoavWeather (data) {
