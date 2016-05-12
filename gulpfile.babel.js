5a2775d5ec1b61350d88089d70861a445a841226
Zack Boehm <git@zackboehm.com>
2016-05-12 01:16:55 -0400

    Add cache busting to gulp build :boom:

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -56,6 +56,7 @@ gulp.task('html', ['styles', 'scripts'], () => {
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano()))
+    .pipe($.if('*.html', $.cacheBust()))
     .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
